c8017eac86b432153d1ba51ec1e62a162f191692
Aryan-max-bit <aryanbhushan2007@gmail.com>
2025-08-19 09:46:49 +0530

    Add files via upload

---
+++ sum of 1st n odd no.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+void main(){
+    int n;
+    printf("enter value of n\n");
+    scanf("%d",&n);
+    int sum=0;
+     for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                    if(i%2!=0){
+                                sum+=i;
+
+                    }
+
+            }
+
+
+
+     }
+     printf("required sum is %d",sum);
+}
