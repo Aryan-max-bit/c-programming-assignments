7cb725f71aaed5bd149e446a3412cf8d28184a7b
Aryan-max-bit <aryanbhushan2007@gmail.com>
2025-08-29 10:26:35 +0530

    Add files via upload

---
+++ integer 1 to 100  divisbible by 5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    int n;
+    printf("enter value of n\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(i%5==0){
+            printf("%d\n",i);
+        }
+    }
+}
