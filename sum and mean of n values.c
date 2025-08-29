6cfe900e2a6854e5d1214deb7ff9f0e62acc991d
Aryan-max-bit <aryanbhushan2007@gmail.com>
2025-08-29 09:11:46 +0530

    Add files via upload

---
+++ sum and mean of n values.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+void main(){
+    int n;
+    printf("enter value of n\n");
+    scanf("%d",&n);
+    int sum=0;
+    for(int i=1;i<=n;i++){
+            sum+=i;
+
+    }
+    printf("required sum value is:%d\n",sum);
+    int mean=sum/n;
+    printf("required mean is:%d",mean);
+
+}
+
