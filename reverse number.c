06682768c56646f9377541ea80b7307f862e3222
Aryan-max-bit <aryanbhushan2007@gmail.com>
2025-08-30 23:23:35 +0530

    Add files via upload

---
+++ reverse number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+void main(){
+   int n;
+   printf("enter value of n\n");
+   scanf("%d",&n);
+  while(n>0){
+        int d=n%10;
+    printf("%d",d);
+  n=n/10;
+
+  }
+
+
+    }
+
+
+
