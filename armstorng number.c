27647100e57c6822b07d532e3c4a0bd58e58f3ce
Aryan-max-bit <aryanbhushan2007@gmail.com>
2025-10-31 21:51:37 +0530

    Update armstorng number.c

--- armstorng number.c
+++ armstorng number.c
@@ -1,16 +1,29 @@
-#include<stdio.h>
 
 
-void main(){
-    int n;
-    printf("enter value of n\n");
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        if(n%i==0){
-            printf("%d\n",i);
-        }
+
+
+#include <stdio.h>
+// basically calculating armstrong'
+
+int main() {
+    int n, temp, digit;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;   
+
+    while (temp > 0) {
+        digit = temp % 10;           // separating last digit
+        sum = sum + (digit * digit * digit);  // cube of digit
+        temp = temp / 10;            // removing last digit
     }
 
+    if (sum == n)
+        printf("%d is an Armstrong number\n", n);
+    else
+        printf("%d is not an Armstrong number\n", n);
+
+    return 0;
 }
-
-
